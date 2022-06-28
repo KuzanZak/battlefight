d50e3218436add3b9e2ed03471b5a3953ba4a211
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-28 09:54:25 +0200

    js link img click

--- js/script.js
+++ js/script.js
@@ -62,109 +62,58 @@ const listCharacteristics = document.getElementById("list-fighters-caracteristic
 //         });      
 // };
 
-
-
 function getImageFighter(array){
     array.forEach(fighter => {
         const eachChamp = document.createElement("li");
         listChamps.appendChild(eachChamp);
         eachChamp.setAttribute("data-id", fighter.id);
         eachChamp.classList.add("list-champs-item");
-        eachChamp.innerHTML = `<a class="link-img" href="#"><img class="image-xs" src="${fighter.images.xs}"></a><p class="displayN">${fighter.name}</p>`
+        eachChamp.innerHTML = `<a class="link-img" href="#"><img class="image-xs" src="${fighter.images.xs}"></a>`
+        // eachChamp.innerHTML = `<button class="link-img"><img class="image-xs" src="${fighter.images.xs}"></button>`
     })
 
     listChamps.addEventListener("click", function(event) {
-        if (event.target.classList.contains("list-champs-item")) {
-            document.getElementById("list-fighters-battle").appendChild(listFighters);
-            const eachFighters = document.createElement("li");
-            listFighters.appendChild(eachFighters);
-            eachFighters.classList.add("list-champs-item");
-            const a = event.target.firstElementChild; 
-            eachFighters.innerHTML = `<a class ="link-imgM" href="#"><img class="image-xs-middle" src="${a.firstElementChild.src}"></a>`
-            console.log(event.target.dataset.id)
+        let li,
+        img; 
 
-            async function waitingForResponseId() {
-                const response = await fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${event.target.dataset.id}.json`)
-                const fighterCList = await response.json();
-                const eachChampName = document.createElement("li");
-                listCharacteristics.appendChild(eachChampName);
-                eachChampName.innerHTML = `${fighterCList.name} : force:  ${fighterCList.powerstats.strength},
-                combat :  ${fighterCList.powerstats.combat}, durabilité:  ${fighterCList.powerstats.durability},
-                power:  ${fighterCList.powerstats.power}`               
-            }
-            waitingForResponseId()
+        if (event.target.hasAttribute("src")) {
+            li = event.target.parentElement.parentElement;
+            img = event.target;
+            
+        } else if (event.target.hasAttribute("href")){
+            li = event.target.parentElement;
+            img = event.target.firstElementChild;
+            
+        }
 
-        };
+        document.getElementById("list-fighters-battle").appendChild(listFighters);
+        const eachFighters = document.createElement("li");
+        listFighters.appendChild(eachFighters);
+        eachFighters.classList.add("list-champs-item");
+        eachFighters.innerHTML = `<a class ="link-imgM" href="#"><img class="image-xs-middle" src="${img.src}"></a>`
+    
+        async function waitingForResponseId() {
+            const response = await fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${li.dataset.id}.json`)
+            const fighterCList = await response.json();
+            const eachChampName = document.createElement("li");
+            listCharacteristics.appendChild(eachChampName);
+            eachChampName.innerHTML = `${fighterCList.name} : force:  ${fighterCList.powerstats.strength},
+            combat :  ${fighterCList.powerstats.combat}, durabilité:  ${fighterCList.powerstats.durability},
+            power:  ${fighterCList.powerstats.power}`               
+        }
+        waitingForResponseId()
+            
+        li.remove()
     });
     
     listFighters.addEventListener("click", function(event){
             if(event.target.classList.contains("image-xs-middle")) {
-                        const aM = event.target.parentElement;
-                        const liM = aM.parentElement;
-                        const firstChild = listChamps.firstElementChild; 
-                        listChamps.insertBefore(liM, firstChild)
-                        event.target.classList.replace("image-xs-middle", "image-xs")
-                    }
-                });      
-        };
-        
-        
-        
-
-        
-        
-        // // FIGHT BATTLE
-        // function getRandomValue(max){
-            //     return parseInt(Math.random()*max)
-            // }
-            
-            // // Récupérer le score d'attaque d'un personnage
-            // function getAttackScore(fighter){
-                //     return fighter.strength + getRandomValue(fighter.combat)
-                // }
-                
-                // // Récupérer le score de défense d'un joueur
-                // function getDefenseScore(fighter){
-                    //     return fighter.strength + getRandomValue(fighter.durability)
-                    // } 
-
-// // Baisser les points de power d'un joueur (vie)
-// function decreasePower(fighter, value){
-    //     fighter.power -= value;
-    //     return fighter.power;
-    // }
-    
-    // function getOutNoob(){
-        //     fighterList = fighterList.filter(fighter => fighter.power > 0);
-        // }
-        
-        // // Faire s'affronter 2 joueurs 
-        // function fighter (a, d){
-            //     const attackScore = getAttackScore(a);
-            //     if (attackScore > getDefenseScore(d)) {
-                //         decreaseLife(d, attackScore);
-//         buryTheDeads();
-//         return true;
-//     }
-//     return false;
-// }
-
-// function battle(){
-//     const attacker = getRandomChar();
-//     const defender = getRandomChar(attacker);
-//     console.log(`${attacker.name} is attacking ${defender.name}.`);
-    
-//     const resultText = fight(attacker, defender) ? "win" : "lose";
-//     console.log(`${attacker.name} ${resultText}.`);
-    
-//     console.table(fighterList);
-    
-//     if (fighterList.length <= 1) {
-//         console.table(fighterList);
-//         console.log(`The winner is ${fighterList[0].name}.`);
-//         return;
-//     }
-//     return battle();
-// }
-
-// battle();
+                    const aM = event.target.parentElement;
+                    const liM = aM.parentElement;
+                    const firstChild = listChamps.firstElementChild; 
+                    listChamps.insertBefore(liM, firstChild)
+                    event.target.classList.replace("image-xs-middle", "image-xs")
+                    // eachChampName.innerHTML="";
+            }
+        });
+};
