70f9a21b6ce04fca07758e2f20ceb18246c7c392
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-28 13:08:55 +0200

    css

--- js/script.js
+++ js/script.js
@@ -32,23 +32,23 @@ const listCharacteristics = document.getElementById("list-fighters-caracteristic
 //         eachChamp.innerHTML = `<a class="link-img" href="#"><img class="image-xs" src="${fighter.images.xs}"></a><p class="displayN">${fighter.name}</p>`
 //     })
 
-//     listChamps.addEventListener("click", function(event) {
-//         if (event.target.classList.contains("image-xs")) {
-//             document.getElementById("list-fighters-battle").appendChild(listFighters);
-//             const eachFighters = document.createElement("li");
-//             listFighters.appendChild(eachFighters);
-//             eachFighters.classList.add("list-champs-item");
-//             eachFighters.innerHTML = `<a class ="link-imgM" href="#"><img class="image-xs-middle" src="${event.target.src}"></a>`
-//             const a = event.target.parentElement
-//             const li = a.parentElement
+// listChamps.addEventListener("click", function(event) {
+//     if (event.target.classList.contains("image-xs")) {
+//         document.getElementById("list-fighters-battle").appendChild(listFighters);
+//         const eachFighters = document.createElement("li");
+//         listFighters.appendChild(eachFighters);
+//         eachFighters.classList.add("list-champs-item");
+//         eachFighters.innerHTML = `<a class ="link-imgM" href="#"><img class="image-xs-middle" src="${event.target.src}"></a>`
+//         const a = event.target.parentElement
+//         const li = a.parentElement
 
-//             const eachChampName = document.createElement("li");
-//             listCharacteristics.appendChild(eachChampName);
-//             eachChampName.innerHTML = `<p>${li.lastChild.innerText}</p>`;
+//         const eachChampName = document.createElement("li");
+//         listCharacteristics.appendChild(eachChampName);
+//         eachChampName.innerHTML = `<p>${li.lastChild.innerText}</p>`;
 
-//             li.remove();
-//         };
-//     });
+//         li.remove();
+//     };
+// });
 
 //     listFighters.addEventListener("click", function(event){
 //         if(event.target.classList.contains("image-xs-middle")) {
@@ -62,33 +62,32 @@ const listCharacteristics = document.getElementById("list-fighters-caracteristic
 //         });      
 // };
 
+let eachChampName;
 function getImageFighter(array){
+    let string ="";
     array.forEach(fighter => {
-        const eachChamp = document.createElement("li");
-        listChamps.appendChild(eachChamp);
-        eachChamp.setAttribute("data-id", fighter.id);
-        eachChamp.classList.add("list-champs-item");
-        eachChamp.innerHTML = `<a class="link-img" href="#"><img class="image-xs" src="${fighter.images.xs}"></a>`
-        // eachChamp.innerHTML = `<button class="link-img"><img class="image-xs" src="${fighter.images.xs}"></button>`
-    })
-    addImageSkills();
-    removeImg();
+        string += `<li class="list-champs-item" data-id="${fighter.id}"><a class="link-img" href="#"><img class="image-xs" src="${fighter.images.sm}"></a></li>`;
+    });
+    listChamps.innerHTML = string;
+    getFighter();
+    removeFighter();
 };
 
 
-function removeImg(){
+function removeFighter(){
     listFighters.addEventListener("click", function(event){
         if(event.target.classList.contains("image-xs-middle")) {
             const aM = event.target.parentElement;
             const liM = aM.parentElement;
             const firstChild = listChamps.firstElementChild; 
             listChamps.insertBefore(liM, firstChild)
-            event.target.classList.replace("image-xs-middle", "image-xs")
+            event.target.classList.replace("image-xs-middle", "image-xs");
+            eachChampName.innerHTML ="";
         }
     });
 }
 
-function addImageSkills(){
+function getFighter(){
     listChamps.addEventListener("click", function(event) {
         let li,
         img; 
@@ -96,13 +95,10 @@ function addImageSkills(){
         if (event.target.hasAttribute("src")) {
             li = event.target.parentElement.parentElement;
             img = event.target;
-            console.log(img)
             
         } else if (event.target.hasAttribute("href")){
             li = event.target.parentElement;
-            img = event.target.firstElementChild;
-            console.log(img)
-            
+            img = event.target.firstElementChild;          
         }
     
         document.getElementById("list-fighters-battle").appendChild(listFighters);
@@ -114,7 +110,7 @@ function addImageSkills(){
         async function waitingForResponseId() {
             const response = await fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${li.dataset.id}.json`)
             const fighterCList = await response.json();
-            addRemoveText(fighterCList);        
+            getCharacteristics(fighterCList);        
         }
         waitingForResponseId()
             
@@ -122,12 +118,7 @@ function addImageSkills(){
     });
 }
 
-
-function addRemoveText(array){
-    const eachChampName = document.createElement("li");
-    eachChampName.classList.add("skills-fighters")
-    listCharacteristics.appendChild(eachChampName);
-    eachChampName.innerHTML = `${array.name} : force:  ${array.powerstats.strength},
-    combat :  ${array.powerstats.combat}, durabilité:  ${array.powerstats.durability},
-    power:  ${array.powerstats.power}`; 
+function getCharacteristics(array){
+    listCharacteristics.innerHTML += `<li class="skills-fighters"><p class="characterisctis-name">${array.name}</p><p class="skills-ttl"><span class="span-ttl">Force :</span> ${array.powerstats.strength}</p>
+    <p class="skills-ttl"><span class="span-ttl">Combat :</span>  ${array.powerstats.combat}</p><p class="skills-ttl"><span class="span-ttl">Power :</span> ${array.powerstats.durability}</p><p class="skills-ttl"><span class="span-ttl">Power :</span>  ${array.powerstats.power}</p></li>`; 
 };
