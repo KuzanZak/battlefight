43965c7d3101b287c048ed1a0160735dee42b5fb
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-28 10:43:38 +0200

    add functions js

--- js/script.js
+++ js/script.js
@@ -71,21 +71,40 @@ function getImageFighter(array){
         eachChamp.innerHTML = `<a class="link-img" href="#"><img class="image-xs" src="${fighter.images.xs}"></a>`
         // eachChamp.innerHTML = `<button class="link-img"><img class="image-xs" src="${fighter.images.xs}"></button>`
     })
+    addImageSkills();
+    removeImg();
+};
 
+
+function removeImg(){
+    listFighters.addEventListener("click", function(event){
+        if(event.target.classList.contains("image-xs-middle")) {
+            const aM = event.target.parentElement;
+            const liM = aM.parentElement;
+            const firstChild = listChamps.firstElementChild; 
+            listChamps.insertBefore(liM, firstChild)
+            event.target.classList.replace("image-xs-middle", "image-xs")
+        }
+    });
+}
+
+function addImageSkills(){
     listChamps.addEventListener("click", function(event) {
         let li,
         img; 
-
+    
         if (event.target.hasAttribute("src")) {
             li = event.target.parentElement.parentElement;
             img = event.target;
+            console.log(img)
             
         } else if (event.target.hasAttribute("href")){
             li = event.target.parentElement;
             img = event.target.firstElementChild;
+            console.log(img)
             
         }
-
+    
         document.getElementById("list-fighters-battle").appendChild(listFighters);
         const eachFighters = document.createElement("li");
         listFighters.appendChild(eachFighters);
@@ -95,25 +114,20 @@ function getImageFighter(array){
         async function waitingForResponseId() {
             const response = await fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${li.dataset.id}.json`)
             const fighterCList = await response.json();
-            const eachChampName = document.createElement("li");
-            listCharacteristics.appendChild(eachChampName);
-            eachChampName.innerHTML = `${fighterCList.name} : force:  ${fighterCList.powerstats.strength},
-            combat :  ${fighterCList.powerstats.combat}, durabilité:  ${fighterCList.powerstats.durability},
-            power:  ${fighterCList.powerstats.power}`               
+            addRemoveText(fighterCList);        
         }
         waitingForResponseId()
             
         li.remove()
     });
-    
-    listFighters.addEventListener("click", function(event){
-            if(event.target.classList.contains("image-xs-middle")) {
-                    const aM = event.target.parentElement;
-                    const liM = aM.parentElement;
-                    const firstChild = listChamps.firstElementChild; 
-                    listChamps.insertBefore(liM, firstChild)
-                    event.target.classList.replace("image-xs-middle", "image-xs")
-                    // eachChampName.innerHTML="";
-            }
-        });
+}
+
+
+function addRemoveText(array){
+    const eachChampName = document.createElement("li");
+    eachChampName.classList.add("skills-fighters")
+    listCharacteristics.appendChild(eachChampName);
+    eachChampName.innerHTML = `${array.name} : force:  ${array.powerstats.strength},
+    combat :  ${array.powerstats.combat}, durabilité:  ${array.powerstats.durability},
+    power:  ${array.powerstats.power}`; 
 };
