ceef7b1a041e946ff75fef684ee1f4c6d2d7d678
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-28 16:58:15 +0200

    oh shit, here we go again

--- js/script.js
+++ js/script.js
@@ -11,14 +11,6 @@ async function waitingForResponseId(id) {
     const fighterCList = await response.json();
     getCharacteristics(fighterCList);        
 }
-// async function waitingForResponseId() {
-//     const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/1.json")
-//     const fighterCList = await response.json();
-//     // getImageFighter(fighterList);
-//     // getCharacteristics(fighterList)
-//     console.log(fighterCList)
-// }
-// waitingForResponseId()
 
     /* Image */ 
 
@@ -28,46 +20,6 @@ const listFighters = document.getElementById("list-fighters")
 
 const listCharacteristics = document.getElementById("list-fighters-caracteristics");
 
-
-// function getImageFighter(array){
-//     array.forEach(fighter => {
-//         const eachChamp = document.createElement("li");
-//         listChamps.appendChild(eachChamp);
-//         eachChamp.classList.add("list-champs-item");
-//         eachChamp.innerHTML = `<a class="link-img" href="#"><img class="image-xs" src="${fighter.images.xs}"></a><p class="displayN">${fighter.name}</p>`
-//     })
-
-// listChamps.addEventListener("click", function(event) {
-//     if (event.target.classList.contains("image-xs")) {
-//         document.getElementById("list-fighters-battle").appendChild(listFighters);
-//         const eachFighters = document.createElement("li");
-//         listFighters.appendChild(eachFighters);
-//         eachFighters.classList.add("list-champs-item");
-//         eachFighters.innerHTML = `<a class ="link-imgM" href="#"><img class="image-xs-middle" src="${event.target.src}"></a>`
-//         const a = event.target.parentElement
-//         const li = a.parentElement
-
-//         const eachChampName = document.createElement("li");
-//         listCharacteristics.appendChild(eachChampName);
-//         eachChampName.innerHTML = `<p>${li.lastChild.innerText}</p>`;
-
-//         li.remove();
-//     };
-// });
-
-//     listFighters.addEventListener("click", function(event){
-//         if(event.target.classList.contains("image-xs-middle")) {
-//                 const aM = event.target.parentElement;
-//                 const liM = aM.parentElement;
-//                 const firstChild = listChamps.firstElementChild; 
-//                 // console.log(firstChild);
-//                 listChamps.insertBefore(liM, firstChild)
-//                 event.target.classList.replace("image-xs-middle", "image-xs")
-//             }
-//         });      
-// };
-
-let eachChampName;
 function getImageFighter(array){
     let string ="";
     array.forEach(fighter => {
@@ -87,7 +39,13 @@ function removeFighter(){
             const firstChild = listChamps.firstElementChild; 
             listChamps.insertBefore(liM, firstChild)
             event.target.classList.replace("image-xs-middle", "image-xs");
-            eachChampName.innerHTML ="";
+            
+            document.querySelectorAll(".skills-fighters").forEach(skills => {
+                if(skills.dataset.name == skills.firstElementChild.innerText) {
+                    // skills.hasAttribute("data-name").remove();
+                    console.log(skills)
+                }
+            })  
         }
     });
 }
@@ -119,6 +77,6 @@ function getFighter(){
 
 
 function getCharacteristics(array){
-    listCharacteristics.innerHTML += `<li class="skills-fighters"><p class="characterisctis-name">${array.name}</p><p class="skills-ttl"><span class="span-ttl">Force :</span> ${array.powerstats.strength}</p>
+    listCharacteristics.innerHTML += `<li data-name="${array.name}" class="skills-fighters"><p class="characterisctis-name">${array.name}</p><p class="skills-ttl"><span class="span-ttl">Force :</span> ${array.powerstats.strength}</p>
     <p class="skills-ttl"><span class="span-ttl">Combat :</span>  ${array.powerstats.combat}</p><p class="skills-ttl"><span class="span-ttl">Power :</span> ${array.powerstats.durability}</p><p class="skills-ttl"><span class="span-ttl">Power :</span>  ${array.powerstats.power}</p></li>`; 
 };
